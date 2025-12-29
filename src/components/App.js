7e79277b451f8acc4264754069f57e89fd243696
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-29 15:25:49 +0530

    Integrate Review component into App
    Added Review component to the App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,18 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import Review from "./Review";
+import './../styles/App.css';
+
+const App = () => {
+  return (
+    <main>
+      <section className="container">
+        <h2 id="review-heading">Our Reviews</h2>
+        {/* Render the Review component */}
+        <Review />
+      </section>
+    </main>
+  );
+};
+
+export default App;
 
